7e540357fb7627ddba06b965d288f384bc65814b
SneakyBean88 <61949005+SneakyBean88@users.noreply.github.com>
2024-02-14 08:41:49 -0500

    Add login and create_user routes

--- Django/Jewish_Positivity_Django/urls.py
+++ Django/Jewish_Positivity_Django/urls.py
@@ -1,12 +1,14 @@
 # in urls.py
 
-from .views import login_view, logout_view
+from .views import login_view, create_user_view, logout_view
 from django.urls import include, path
 from django.contrib import admin
 
 urlpatterns = [
-    #path('login/', login_view, name='login'),
-    #path('logout/', logout_view, name='logout'),
+    path('login/', login_view, name='login'),
+    path('create_user/', create_user_view, name='create_user'),
+    # path('logout/', logout_view, name='logout'),
     path('admin/', admin.site.urls),
-    #path('timer/', include('timer.urls')),
+
+    # path('Jewish_Positivity_Django/', include('Jewish_Positivity_Django.urls')),
 ]
