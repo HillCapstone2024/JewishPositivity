c851f8022a83f5520a127d948f4e551e9c9bcc8d
Alex Gallant <anakingallant@gmail.com>
2024-02-28 08:37:07 -0500

    Making the django key private :)

--- Django/Jewish_Positivity_Django/settings.py
+++ Django/Jewish_Positivity_Django/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-7meonev(26)4sbt7v-(3+mjf%r3yh$+kmalikrjo=lr3#vt21_"
+SECRET_KEY = os.getenv("DJ_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
