8af8bac3aea3737bf017f3668fb85ffe25276c9a
Rybeardawg1 <104077296+Rybeardawg1@users.noreply.github.com>
2024-03-19 13:12:30 -0400

    starting archive.js

--- React_Native/screens/home/Archive.js
+++ React_Native/screens/home/Archive.js
@@ -1,14 +1,75 @@
-import * as React from 'react';
-import { View, Text, Alert } from 'react-native';
+import React, { useEffect, useState } from "react";
+import { View, Text, Alert, ActivityIndicator, StyleSheet } from 'react-native';
 import makeThemeStyle from '../../Theme.js';
+import IP_ADDRESS from "../../ip.js";
+const API_URL = "http://" + IP_ADDRESS + ":8000";
+import axios from 'axios';
 
-export default function Archive({navigaton}) {
+export default function Archive({ navigaton }) {
+    const [username, setUsername] = useState("");
+    const [entries, setEntries] = useState([]);
+    const [message, setMessage] = useState(<ActivityIndicator />);
     theme = makeThemeStyle();
-    return(
-        <View style={[{ flex: 1, alignItems: 'center', justifyContent: 'center'}, theme['background']]}>
-            <Text
-                onPress={() => alert('This is the "Archive" screen ')}
-                style={[{ fontSize: 26, fontWeight: 'bold' }, theme['color']]}>Archive</Text>
-        </View>
+    const index = 0;
+
+    handleGetEntries = async () => {
+        const getCsrfToken = async () => {
+            try {
+                const response = await axios.get(`${API_URL}/csrf-token/`);
+                return response.data.csrfToken;
+            } catch (error) {
+                console.error("Error retrieving CSRF token:", error);
+                throw new Error("CSRF token retrieval failed");
+            }
+        };
+
+        try {
+            const csrfToken = await getCsrfToken();
+            const response = await axios.post(
+                `${API_URL}/get_entries/`,
+                {
+                    username: username,
+                    index: index,
+                },
+                {
+                    headers: {
+                        "X-CSRFToken": csrfToken,
+                        "Content-Type": "application/json",
+                    },
+                    withCredentials: true,
+                }
+            );
+            console.log("Axios response:", response.data);
+        } catch (error) {
+            console.log(error);
+            console.error("Entry Retrieval Error:", error.response.data);
+        }
+    };
+    handleGetEntries();
+
+    useEffect(() => {
+        if (entries.length == 0) {
+            setMessage(<Text style={[styles.title, theme['color']]}>No entries yet!</Text>);
+        }
+    }, []);
+
+    return (
+        <View style={[{ flex: 1, alignItems: 'center', justifyContent: 'center' }, theme['background']]}>
+            {message}
+        </View >
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    title: {
+        fontSize: 30,
+        fontWeight: "bold",
+        marginBottom: 20,
+        color: "white",
+    },
+});
