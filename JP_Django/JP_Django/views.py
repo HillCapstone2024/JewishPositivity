13504bd212d9b17b0a1b1831dbb76e9d1d81432b
Roman Galeano <romanag556@gmail.com>
2024-03-19 16:46:22 -0400

    added a few more adjustments

--- JP_Django/JP_Django/views.py
+++ JP_Django/JP_Django/views.py
@@ -211,100 +211,7 @@ def logout_view(request):
 
 
 # ########## User Information Management ##########
-       
-def update_times_view(request):
-    if request.method == "POST":
-        # Retrieving username to access correct user in database
-        # retrieving times for updating to non-default
-        data = json.loads(request.body)
-        username = data.get("username")
-        time1_str = data.get("time1")
-        time2_str = data.get("time2")
-        time3_str = data.get("time3")
-        logging.info("TIMES: TIME1: %s,TIME2: %s,TIME3: %s", time1_str, time2_str, time3_str)
-
-        try:
-            # Convert strings of posted times to time objects
-            time1 = time.fromisoformat(time1_str)  # fromisoformat() expects format ("HH:MM:SS")
-            time2 = time.fromisoformat(time2_str)
-            time3 = time.fromisoformat(time3_str)
-            if time1 < time2 < time3:
-                user = User.objects.get(username=username)  # Retrieving user from the database
-                # Get the user's timezone and current date
-                user_timezone = pytz.timezone(user.timezone) # Get user's timezone
-                current_date = datetime.datetime.now().date() # Get current date
-
-                # Set the times to UTC
-                datetime1_current = datetime.datetime.combine(current_date, time1) # Combine date and time to get local datetime
-                datetime1_local = user_timezone.localize(datetime1_current, is_dst=None)
-                datetime1_utc = datetime1_local.astimezone(pytz.utc)
-
-                datetime2_current = datetime.datetime.combine(current_date, time2) # Combine date and time to get local datetime
-                datetime2_local = user_timezone.localize(datetime2_current, is_dst=None)
-                datetime2_utc = datetime2_local.astimezone(pytz.utc)
-                
-                datetime3_current = datetime.datetime.combine(current_date, time3) # Combine date and time to get local datetime
-                datetime3_local = user_timezone.localize(datetime3_current, is_dst=None)
-                datetime3_utc = datetime3_local.astimezone(pytz.utc)
-
-                # Set the updated times to the user
-                user.time1 = datetime1_utc.strftime("%Y-%m-%d %H:%M:%S")  # Convert datetime objects to strings
-                user.time2 = datetime2_utc.strftime("%Y-%m-%d %H:%M:%S")
-                user.time3 = datetime3_utc.strftime("%Y-%m-%d %H:%M:%S")
-                user.save()  # Saving
-
-                response_data = {"message": "Success! Times have been updated"}
-                return HttpResponse(json.dumps(response_data), content_type="application/json")
-            else:
-                return HttpResponse("Invalid ordering", status=400)
-        except User.DoesNotExist:
-            return HttpResponse("User not found", status=400)
-        except ValueError:
-            return HttpResponse("Invalid time format", status=400)
-        except Exception as e:
-            return HttpResponse("Updating user times failed: " + str(e), status=400)
-    return HttpResponse(constNotPost)
-
-
-def get_times_view(request):
-    if request.method == "GET":
-        username = request.GET.get("username")  # JSON is not typically used for GET requests here
-
-        # Make sure the get data is not empty
-        if username is not None:
-            try:
-                # Retrieve the user from the database by username
-                user = User.objects.get(username=username)
-
-                # get the times as UTC + timezone
-                time1_utc = user.time1.time()
-                time2_utc = user.time2.time()
-                time3_utc = user.time3.time()
-                user_timezone = pytz.timezone(user.timezone) # Get timezone object
-                current_date = datetime.datetime.now().date() # Get current date
-                # Set the times to user's timezone
-                datetime1_utc = datetime.datetime.combine(current_date, time1_utc) # Combine date and time to get UTC datetime
-                datetime1 = datetime1_utc.astimezone(user_timezone) # Convert to timezone
-                datetime2_utc = datetime.datetime.combine(current_date, time2_utc) # Combine date and time to get UTC datetime
-                datetime2 = datetime2_utc.astimezone(user_timezone) # Convert to timezone
-                datetime3_utc = datetime.datetime.combine(current_date, time3_utc) # Combine date and time to get UTC datetime
-                datetime3 = datetime3_utc.astimezone(user_timezone) # Convert to timezone
-                response_data = {
-                    "time1": datetime1.time().strftime("%H:%M:%S"),
-                    "time2": datetime2.time().strftime("%H:%M:%S"),
-                    "time3": datetime3.time().strftime("%H:%M:%S"),
-                }
-
-                logging.info(response_data)
-                return HttpResponse(response_data)  # returning a DICTIONARY - do not change
-            except Exception as e:
-                logging.info(e)
-                return HttpResponse("User does not exist", status=400)
-        else:  # username was empty
-            return HttpResponse("Username not provided", status=400)
-    return HttpResponse("Not a GET request!")
-
-
+    
 def update_user_information_view(request):
     # Check if the request method is POST
     if request.method != "POST":
@@ -433,6 +340,98 @@ def get_user_information_view(request):
         else:  # username was empty
             return HttpResponse("Username not provided", status=400)
     return HttpResponse("Not a GET request")
+       
+def update_times_view(request):
+    if request.method == "POST":
+        # Retrieving username to access correct user in database
+        # retrieving times for updating to non-default
+        data = json.loads(request.body)
+        username = data.get("username")
+        time1_str = data.get("time1")
+        time2_str = data.get("time2")
+        time3_str = data.get("time3")
+        logging.info("TIMES: TIME1: %s,TIME2: %s,TIME3: %s", time1_str, time2_str, time3_str)
+
+        try:
+            # Convert strings of posted times to time objects
+            time1 = time.fromisoformat(time1_str)  # fromisoformat() expects format ("HH:MM:SS")
+            time2 = time.fromisoformat(time2_str)
+            time3 = time.fromisoformat(time3_str)
+            if time1 < time2 < time3:
+                user = User.objects.get(username=username)  # Retrieving user from the database
+                # Get the user's timezone and current date
+                user_timezone = pytz.timezone(user.timezone) # Get user's timezone
+                current_date = datetime.datetime.now().date() # Get current date
+
+                # Set the times to UTC
+                datetime1_current = datetime.datetime.combine(current_date, time1) # Combine date and time to get local datetime
+                datetime1_local = user_timezone.localize(datetime1_current, is_dst=None)
+                datetime1_utc = datetime1_local.astimezone(pytz.utc)
+
+                datetime2_current = datetime.datetime.combine(current_date, time2) # Combine date and time to get local datetime
+                datetime2_local = user_timezone.localize(datetime2_current, is_dst=None)
+                datetime2_utc = datetime2_local.astimezone(pytz.utc)
+                
+                datetime3_current = datetime.datetime.combine(current_date, time3) # Combine date and time to get local datetime
+                datetime3_local = user_timezone.localize(datetime3_current, is_dst=None)
+                datetime3_utc = datetime3_local.astimezone(pytz.utc)
+
+                # Set the updated times to the user
+                user.time1 = datetime1_utc.strftime("%Y-%m-%d %H:%M:%S")  # Convert datetime objects to strings
+                user.time2 = datetime2_utc.strftime("%Y-%m-%d %H:%M:%S")
+                user.time3 = datetime3_utc.strftime("%Y-%m-%d %H:%M:%S")
+                user.save()  # Saving
+
+                response_data = {"message": "Success! Times have been updated"}
+                return HttpResponse(json.dumps(response_data), content_type="application/json")
+            else:
+                return HttpResponse("Invalid ordering", status=400)
+        except User.DoesNotExist:
+            return HttpResponse("User not found", status=400)
+        except ValueError:
+            return HttpResponse("Invalid time format", status=400)
+        except Exception as e:
+            return HttpResponse("Updating user times failed: " + str(e), status=400)
+    return HttpResponse(constNotPost)
+
+
+def get_times_view(request):
+    if request.method == "GET":
+        username = request.GET.get("username")  # JSON is not typically used for GET requests here
+
+        # Make sure the get data is not empty
+        if username is not None:
+            try:
+                # Retrieve the user from the database by username
+                user = User.objects.get(username=username)
+
+                # get the times as UTC + timezone
+                time1_utc = user.time1.time()
+                time2_utc = user.time2.time()
+                time3_utc = user.time3.time()
+                user_timezone = pytz.timezone(user.timezone) # Get timezone object
+                current_date = datetime.datetime.now().date() # Get current date
+                # Set the times to user's timezone
+                datetime1_utc = datetime.datetime.combine(current_date, time1_utc) # Combine date and time to get UTC datetime
+                datetime1 = datetime1_utc.astimezone(user_timezone) # Convert to timezone
+                datetime2_utc = datetime.datetime.combine(current_date, time2_utc) # Combine date and time to get UTC datetime
+                datetime2 = datetime2_utc.astimezone(user_timezone) # Convert to timezone
+                datetime3_utc = datetime.datetime.combine(current_date, time3_utc) # Combine date and time to get UTC datetime
+                datetime3 = datetime3_utc.astimezone(user_timezone) # Convert to timezone
+                response_data = {
+                    "time1": datetime1.time().strftime("%H:%M:%S"),
+                    "time2": datetime2.time().strftime("%H:%M:%S"),
+                    "time3": datetime3.time().strftime("%H:%M:%S"),
+                }
+
+                logging.info(response_data)
+                return HttpResponse(response_data)  # returning a DICTIONARY - do not change
+            except Exception as e:
+                logging.info(e)
+                return HttpResponse("User does not exist", status=400)
+        else:  # username was empty
+            return HttpResponse("Username not provided", status=400)
+    return HttpResponse("Not a GET request!")
 
 
 
