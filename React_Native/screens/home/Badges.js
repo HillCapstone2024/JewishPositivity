142bc8ef49f40af218a66ec92849ffa4d2c7d5e4
JPcheco <124377479+JPcheco@users.noreply.github.com>
2024-03-26 17:50:54 -0400

    Created Badges/Friends file and added them to the drawer

---
+++ React_Native/screens/home/Badges.js
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from 'react';
+import { View, Text, Alert } from 'react-native';
+import makeThemeStyle from '../../Theme.js';
+
+export default function Badges({navigaton}) {
+    theme = makeThemeStyle();
+    return(
+        <View style={[{ flex: 1, alignItems: 'center', justifyContent: 'center'}, theme['background']]}>
+            <Text
+                onPress={() => alert('This is the "Badges" screen ')}
+                style={[{ fontSize: 26, fontWeight: 'bold' }, theme['color']]}>Badges</Text>
+        </View>
+    );
+}
