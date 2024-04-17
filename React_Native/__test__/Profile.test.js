52af133f1acee99b3ab10efb9b998e7bd497eaaa
akoser1 <166929613+akoser1@users.noreply.github.com>
2024-04-17 18:02:17 -0400

    Profile.test.js

---
+++ React_Native/__test__/Profile.test.js
@@ -0,0 +1,39 @@
+//the page renders, username, first name, last name, email is there
+//the profile page is there
+//when edit profile is clicked, it changes to edit page
+//when they press profile change button, alert appears
+//test profile submit change, axios mock call
+//new profile information gets displayed
+
+import React from "react";
+import { fireEvent, render, waitFor } from "@testing-library/react-native";
+import axios from "axios";
+import Profile from "../screens/auth/Profile.js";
+
+//mock axios call
+jest.mock("axios");
+
+//Mocking navigateDrawer
+const mockNavigateDrawer = jest.fn();
+const mockResetNavigation = jest.fn();
+
+jest.mock("@react-native-async-storage/async-storage", () => ({
+  setItem: jest.fn(),
+  getItem: jest.fn(() => Promise.resolve(null)), // Adjust this as needed for your tests
+  removeItem: jest.fn(),
+  clear: jest.fn(),
+}));
+
+describe("Profile Component", () => {
+  it("can do simple jest test", () => {
+    expect(1 + 2).toBe(3);
+  });
+
+  it("Successfully renders username, first name, last name, and email", () => {
+    const {getByTestId} = render(<Profile />);
+    expect(getByTestId("firstnameInput")).toBeTruthy();
+    expect(getByTestId("lastnameInput")).toBeTruthy();
+    expect(getByTestId("usernameInput")).toBeTruthy();
+    expect(getByTestId("emailInput")).toBeTruthy();
+  });
+});
