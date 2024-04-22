124dbb7c53c8476f81c37200b95279f841de3a06
alvingeorge567 <albingeorge567@gmail.com>
2024-04-22 13:35:57 -0400

    Update Profile.test.js

--- React_Native/__test__/Profile.test.js
+++ React_Native/__test__/Profile.test.js
@@ -8,7 +8,7 @@
 import React from "react";
 import { fireEvent, render, waitFor } from "@testing-library/react-native";
 import axios from "axios";
-import Profile from "../screens/auth/Profile.js";
+import Profile from "../screens/home/Profile.js";
 
 //mock axios call
 jest.mock("axios");
@@ -29,6 +29,7 @@ describe("Profile Component", () => {
     expect(1 + 2).toBe(3);
   });
 
+  //the page renders, username, first name, last name, email is there
   it("Successfully renders username, first name, last name, and email", () => {
     const {getByTestId} = render(<Profile />);
     expect(getByTestId("firstnameInput")).toBeTruthy();
@@ -36,4 +37,30 @@ describe("Profile Component", () => {
     expect(getByTestId("usernameInput")).toBeTruthy();
     expect(getByTestId("emailInput")).toBeTruthy();
   });
+
+  //the profile page is there
+  it("Renders the Profile Component", () => {
+    const { getByTestId }= render(<Profile />);
+    expect(getByTestId("profileContainer")).toBeTruthy();
+  });
+
+  //when edit profile is clicked, it changes to edit page
+  it("Navigates to the edit profile page when 'Edit Profile' is clicked", () => {
+    const { getByTestId } = render(<Profile navigation={{ navigate: mockNavigateDrawer }} />);
+    fireEvent.press(getByText("Edit Profile"));
+    expect(mockNavigateDrawer).toHaveBeenCalledWith("EditProfile");
+  });
+
+  //when they press profile change button, alert appears
+  it("Displays an alert when the profile change button is pressed", () => {
+    const { getByText } = render(<Profile />);
+    jest.spyOn(global,'alert').mockImplementation(() => {});
+    fireEvent.press(getByText("Change Profile Picture"));
+    expect(global.alert).toHaveBeenCalledWith("Profile picture changed successfully!");
+    global.alert.mockRestore();
+  });
+
+  //test profile submit change, axios mock call
+  
+  //new profile information gets displayed
 });
